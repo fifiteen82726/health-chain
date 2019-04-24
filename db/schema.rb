c7f5d0203344a19e174e6a1b0072369d8b57e2af
fifiten82726 <fifiteen82726@gmail.com>
2019-04-23 21:05:11 -0400

    Add: ac model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190424010022) do
+ActiveRecord::Schema.define(version: 20190424010407) do
+
+  create_table "acs", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "name"
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_acs_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_acs_on_reset_password_token", unique: true
+  end
 
   create_table "companies", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"

018bd73d690f383b4a0eb0ee5a27102e48b7177b
fifiten82726 <fifiteen82726@gmail.com>
2019-04-24 21:36:00 -0400

    Add: seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.destroy_all
+Ac.destroy_all
+
+
+user = User.create(email: 'user1@gmail.com', password: '123123123')
+ac1 = Ac.create(name: 'Airline_A', email: 'airlinea@gmail.com', password: '123123123')
+ac1 = Ac.create(name: 'Airline_B', email: 'airlineb@gmail.com', password: '123123123')
+
+Order.create(user: user, ac: ac1, seat: '38A', date: DateTime.new(2020,1,1), arrival: 'Toronto', departure: 'Buffalo')
+Order.create(user: user, ac: ac1, seat: '30B', date: DateTime.new(2020,2,1), arrival: 'Chicago', departure: 'Buffalo')
+Order.create(user: user, ac: ac1, seat: '10C', date: DateTime.new(2020,3,1), arrival: 'Boston', departure: 'Buffalo')

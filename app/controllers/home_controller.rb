89c836697c5dd1bed3fd975c7fb2d8728ee6fe4c
fifiten82726 <fifiteen82726@gmail.com>
2019-04-24 22:15:31 -0400

    Add: user, airline basic page and route

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,17 @@
 class HomeController < ApplicationController
+  before_action :authenticate_user!, only: :user_home
+  before_action :authenticate_ac!, only: :acs_home
+
   def index
+    return redirect_to users_home_path if current_user
+    return redirect_to acs_home_path if current_ac
+  end
+
+  def user_home
+    @orders = Order.where(user_id: current_user.id)
+  end
+
+  def acs_home
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_for :acs
   devise_for :users
   root 'home#index'
+  get 'users_home' => 'home#user_home'
+  get 'acs_home' => 'home#acs_home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

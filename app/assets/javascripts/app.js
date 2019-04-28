785396cadf5b99cff5835aa0a04e07f7ce3b863d
fifiten82726 <fifiteen82726@gmail.com>
2019-04-28 15:58:16 -0400

    Add: first confirm mock done

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -36,43 +36,54 @@ App = {
       App.contracts.Election.setProvider(App.web3Provider);
 
       // Use our contract to retrieve and mark the adopted pets
-      return App.markAdopted();
+      // return App.markAdopted();
     });
 
     return App.bindEvents();
   },
 
   bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+    // airline confirm customer's special request
+    $(document).on('click', '#confirm', App.createOrderRequest);
+  },
+
+  // if airline confirm user's request, create a record in blockchain
+  createOrderRequest: function(){
+    var airlineInstance;
+    web3.eth.getAccounts(function(error, accounts) {
+      if (error) {
+        console.log(error);
+      }
+      var account = accounts[0];
+      App.contracts.Election.deployed().then(function(instance) {
+        airlineInstance = instance;
+        // Execute adopt as a transaction by sending account
+        return airlineInstance.createOrderRequest(1, true, "0664441aca014fb2482fb6d412d506391c15e0a10645d1a4ec25869c234de7fb39eb056211a86037663d4440d22455e638394cb4f56a9694a7b89e7577ede2a5");
+      }).then(function(result) {
+        debugger
+        // return App.markAdopted();
+      }).catch(function(err) {
+        console.log(err.message);
+        debugger
+      });
+    });
   },
 
   // fetch data from BlockChain
-  markAdopted: function(adopters, account) {
-    var adoptionInstance;
+  // markAdopted: function(adopters, account) {
+  //   var adoptionInstance;
 
-    App.contracts.Election.deployed().then(function(instance) {
-      adoptionInstance = instance;
-
-      return adoptionInstance.candidatesCount.call();
-    }).then(function(result) {
-      // return BlockChain API request
-      console.log(result);
-    /*
-     * Replace me...
-     */
-   });
-  },
-
-  handleAdopt: function(event) {
-    event.preventDefault();
-
-    var petId = parseInt($(event.target).data('id'));
-
-    /*
-     * Replace me...
-     */
-  }
-
+  //   App.contracts.Election.deployed().then(function(instance) {
+  //     adoptionInstance = instance;
+  //     // return adoptionInstance.candidatesCount.call();
+  //   }).then(function(result) {
+  //     // return BlockChain API request
+  //     console.log(result);
+  //   /*
+  //    * Replace me...
+  //    */
+  //  });
+  // }
 };
 
 $(function() {
